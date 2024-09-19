9b9f85aad7159d52535fddb7100f388fb2105980
Rex Chen <yourfriend5340@gmail.com>
2024-09-19 13:27:35 +0800

    index_paractice

---
+++ JS/index.js
@@ -0,0 +1,2 @@
+$('#HTML_aside').load("./aside.html");
+$('#HTML_section').load("./section.html");
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!--Failed to load resource: the server responded with a status of 404 (Not Found) for favicon，會有錯誤故加第6行-->
+    <link rel="shortcut icon" href="#">
+    <link rel="stylesheet" href="./CSS/index.css?202408141037">
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js?202408141037"
+        integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous">
+    </script>
+    <script src="./JS/index.js" defer></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP及Jquery實作</title>
+</head>
+
+<body>
+    <header>
+        <h1>PHP及Jquery實作練習</h1>
+    </header>
+    <hr>
+    <main>
+        <aside id="HTML_aside" name="HTML_aside">
+        </aside>
+
+        <section id="HTML_section" name="HTML_section">
+        </section>
+    </main>
+
+    <footer>
+        <p>這裡是footer，不知道打什麼</p>
+        <address>
+            就…林園……好鄉下……
+        </address>
+    </footer>
+
+</body>
+
+</html>
